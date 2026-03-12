6cd39dfc1612f03938f35b3387df7085f9fa0eea
elecTibo2025 <lztxd111@gmail.com>
2026-03-11 18:42:56 -0700

    update for adapting Ninja

--- build.ps1
+++ build.ps1
@@ -32,17 +32,6 @@ $BuildDir = "$ProjectRoot\build"
 $ElfFile  = "$BuildDir\$ProjectName.elf"
 $BinFile  = "$BuildDir\$ProjectName.bin"
 
-# --- Auto-find make.exe ------------------------------------------------------
-$Make = (Get-Command make -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Source)
-if (-not $Make) {
-    $candidates = @(
-        "D:\MSYS2\usr\bin\make.exe", "D:\Git\usr\bin\make.exe",
-        "C:\MSYS2\usr\bin\make.exe", "C:\msys64\usr\bin\make.exe"
-    )
-    $Make = $candidates | Where-Object { Test-Path $_ } | Select-Object -First 1
-}
-if (-not $Make) { throw "make.exe not found. Add MSYS2 usr/bin to PATH." }
-
 # --- Fix MinGW linker temp file issue ----------------------------------------
 $env:TMP  = "$env:USERPROFILE\AppData\Local\Temp"
 $env:TEMP = "$env:USERPROFILE\AppData\Local\Temp"
@@ -51,12 +40,12 @@ $env:TEMP = "$env:USERPROFILE\AppData\Local\Temp"
 switch ($Target) {
 
     "build" {
-        if (-not (Test-Path "$BuildDir\Makefile")) {
-            throw "build/Makefile not found. Run cmake first from Git Bash:`n  cmake -B build -DCMAKE_TOOLCHAIN_FILE=cmake/gcc-arm-none-eabi.cmake"
+        if (-not (Test-Path $BuildDir)) {
+            throw "build/ not found. Run cmake first from Git Bash:`n  cmake -B build -DCMAKE_TOOLCHAIN_FILE=cmake/gcc-arm-none-eabi.cmake"
         }
-        Write-Host "[make] Building '$ProjectName'..." -ForegroundColor Cyan
-        & $Make -C $BuildDir
-        if ($LASTEXITCODE -ne 0) { throw "make failed" }
+        Write-Host "[cmake] Building '$ProjectName'..." -ForegroundColor Cyan
+        cmake --build $BuildDir
+        if ($LASTEXITCODE -ne 0) { throw "Build failed" }
 
         Write-Host "[objcopy] $ProjectName.elf -> $ProjectName.bin" -ForegroundColor Cyan
         arm-none-eabi-objcopy -O binary $ElfFile $BinFile
